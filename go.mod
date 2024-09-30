4b7811462fae5832f1427454a7786a420ff099c9
Jace996 <116773059+Jace996@users.noreply.github.com>
2024-09-30 22:00:46 +0800

    new:

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/go-saas/sessions
+
+go 1.20
+
+require github.com/gorilla/securecookie v1.1.2
---
+++ header.go
@@ -0,0 +1,91 @@
+package sessions
+
+import (
+	"net/http"
+	"net/textproto"
+	"strings"
+)
+
+type Header interface {
+	Get(key string) string
+	Values(key string) []string
+	Add(key, value string)
+	Set(key, value string)
+}
+
+func ReadCookie(h Header, name string) (*http.Cookie, error) {
+	for _, c := range readCookies(h, name) {
+		return c, nil
+	}
+	return nil, http.ErrNoCookie
+}
+
+// SetCookie adds a Set-Cookie header to the provided ResponseWriter's headers.
+// The provided cookie must have a valid Name. Invalid cookies may be
+// silently dropped.
+func SetCookie(w Header, cookie *http.Cookie) {
+	if v := cookie.String(); v != "" {
+		w.Add("Set-Cookie", v)
+	}
+}
+
+// readCookies parses all "Cookie" values from the header h and
+// returns the successfully parsed Cookies.
+//
+// if filter isn't empty, only cookies of that name are returned
+func readCookies(h Header, filter string) []*http.Cookie {
+	lines := h.Values("Cookie")
+	if len(lines) == 0 {
+		return []*http.Cookie{}
+	}
+
+	cookies := make([]*http.Cookie, 0, len(lines)+strings.Count(lines[0], ";"))
+	for _, line := range lines {
+		line = textproto.TrimString(line)
+
+		var part string
+		for len(line) > 0 { // continue since we have rest
+			if splitIndex := strings.Index(line, ";"); splitIndex > 0 {
+				part, line = line[:splitIndex], line[splitIndex+1:]
+			} else {
+				part, line = line, ""
+			}
+			part = textproto.TrimString(part)
+			if len(part) == 0 {
+				continue
+			}
+			name, val := part, ""
+			if j := strings.Index(part, "="); j >= 0 {
+				name, val = name[:j], name[j+1:]
+			}
+			if !isCookieNameValid(name) {
+				continue
+			}
+			if filter != "" && filter != name {
+				continue
+			}
+			val, ok := parseCookieValue(val, true)
+			if !ok {
+				continue
+			}
+			cookies = append(cookies, &http.Cookie{Name: name, Value: val})
+		}
+	}
+	return cookies
+}
+
+func parseCookieValue(raw string, allowDoubleQuote bool) (string, bool) {
+	// Strip the quotes, if present.
+	if allowDoubleQuote && len(raw) > 1 && raw[0] == '"' && raw[len(raw)-1] == '"' {
+		raw = raw[1 : len(raw)-1]
+	}
+	for i := 0; i < len(raw); i++ {
+		if !validCookieValueByte(raw[i]) {
+			return "", false
+		}
+	}
+	return raw, true
+}
+func validCookieValueByte(b byte) bool {
+	return 0x20 <= b && b < 0x7f && b != '"' && b != ';' && b != '\\'
+}
